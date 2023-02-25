1fcdf3a0f972d257252ad32d4e74803142a8bf58
Lefteris Vladimiros <spirit@siradio.fm>
2023-02-25 08:48:20 +0200

    Add project files.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,60 @@
+export const METANTHROPES = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+
+METANTHROPES.characteristics = {
+	"body": "METANTHROPES.CharacteristicBody",
+	"mind": "METANTHROPES.CharacteristicMind",
+	"soul": "METANTHROPES.CharacteristicSoul",
+};
+
+METANTHROPES.characteristicsAbbreviations = {
+	"body": "METANTHROPES.CharacteristicBodyAbbr",
+	"mind": "METANTHROPES.CharacteristicMindAbbr",
+	"soul": "METANTHROPES.CharacteristicSoulAbbr",
+};
+
+METANTHROPES.stats = {
+	"pow": "METANTHROPES.StatPow",
+	"end": "METANTHROPES.StatEnd",
+	"ref": "METANTHROPES.StatRef",
+	"per": "METANTHROPES.StatPer",
+	"man": "METANTHROPES.StatMan",
+	"cre": "METANTHROPES.StatCre",
+	"wil": "METANTHROPES.StatWil",
+	"con": "METANTHROPES.StatCon",
+	"awa": "METANTHROPES.StatAwa",
+};
+
+METANTHROPES.statAbbreviations = {
+	"pow": "METANTHROPES.StatPowAbbr",
+	"end": "METANTHROPES.StatEndAbbr",
+	"ref": "METANTHROPES.StatRefAbbr",
+	"per": "METANTHROPES.StatPerAbbr",
+	"man": "METANTHROPES.StatManAbbr",
+	"cre": "METANTHROPES.StatCreAbbr",
+	"wil": "METANTHROPES.StatWilAbbr",
+	"con": "METANTHROPES.StatConAbbr",
+	"awa": "METANTHROPES.StatAwaAbbr",
+};
+
+// METANTHROPES.abilities = {
+//   "str": "METANTHROPES.AbilityStr",
+//   "dex": "METANTHROPES.AbilityDex",
+//   "con": "METANTHROPES.AbilityCon",
+//   "int": "METANTHROPES.AbilityInt",
+//   "wis": "METANTHROPES.AbilityWis",
+//   "cha": "METANTHROPES.AbilityCha"
+// };
+// 
+// METANTHROPES.abilityAbbreviations = {
+//   "str": "METANTHROPES.AbilityStrAbbr",
+//   "dex": "METANTHROPES.AbilityDexAbbr",
+//   "con": "METANTHROPES.AbilityConAbbr",
+//   "int": "METANTHROPES.AbilityIntAbbr",
+//   "wis": "METANTHROPES.AbilityWisAbbr",
+//   "cha": "METANTHROPES.AbilityChaAbbr"
+// };
