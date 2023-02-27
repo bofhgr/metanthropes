6e8f8dd061ace2869a40a0374b38d3ebbba4bc46
Lefteris Vladimiros <spirit@siradio.fm>
2023-02-27 18:30:02 +0200

    1.4.15

---
+++ build/README.md
@@ -0,0 +1,17 @@
+# Metanthropes RPG
+
+![Foundry v10](https://img.shields.io/badge/foundry-v10-green)
+
+This is the official Metanthropes RPG system for Foundry VTT.
+This project is in no way affiliated with or supported by FoundryVTT.
+
+## Based off asacolips boilerplate template for Foundry VTT.
+
+For the original work please check out https://gitlab.com/asacolips-projects/foundry-mods/boilerplate
+Using this as a template for Metanthropes RPG system as we learn the ropes of FoundryVTT
+
+### Metanthropes
+
+For more information about Metanthropes RPG visit https://metanthropes.com
+
+![image](https://content.invisioncic.com/e290497/monthly_2022_12/01.jpg.10f501a62b5254cef6f04d9f87c8b52d.jpg)
---
+++ build/module/helpers/config.mjs
@@ -0,0 +1,42 @@
+export const METANTHROPES = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+
+METANTHROPES.characteristics = {
+	"body": "METANTHROPES.CharacteristicBody",
+	"mind": "METANTHROPES.CharacteristicMind",
+	"soul": "METANTHROPES.CharacteristicSoul"
+};
+
+METANTHROPES.characteristicsAbbreviations = {
+	"body": "METANTHROPES.CharacteristicBodyAbbr",
+	"mind": "METANTHROPES.CharacteristicMindAbbr",
+	"soul": "METANTHROPES.CharacteristicSoulAbbr"
+};
+
+METANTHROPES.stats = {
+	"pow": "METANTHROPES.StatPow",
+	"end": "METANTHROPES.StatEnd",
+	"ref": "METANTHROPES.StatRef",
+	"per": "METANTHROPES.StatPer",
+	"man": "METANTHROPES.StatMan",
+	"cre": "METANTHROPES.StatCre",
+	"wil": "METANTHROPES.StatWil",
+	"con": "METANTHROPES.StatCon",
+	"awa": "METANTHROPES.StatAwa"
+};
+
+METANTHROPES.statAbbreviations = {
+	"pow": "METANTHROPES.StatPowAbbr",
+	"end": "METANTHROPES.StatEndAbbr",
+	"ref": "METANTHROPES.StatRefAbbr",
+	"per": "METANTHROPES.StatPerAbbr",
+	"man": "METANTHROPES.StatManAbbr",
+	"cre": "METANTHROPES.StatCreAbbr",
+	"wil": "METANTHROPES.StatWilAbbr",
+	"con": "METANTHROPES.StatConAbbr",
+	"awa": "METANTHROPES.StatAwaAbbr"
+};
